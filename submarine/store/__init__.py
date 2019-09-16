f9f64fc0142617833901026841e7695421cbd105
pingsutw <pingsutw@gmail.com>
2019-09-16 22:35:43 +0800

    Add __init__.py in store dir

--- submarine/store/__init__.py
+++ submarine/store/__init__.py
@@ -1,4 +1,3 @@
-# DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH = "mysql+pymysql://submarine:password@localhost/submarineDB"
-DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH = "mysql+mysqldb://submarine:password@localhost:3306/submarineDB"
+DEFAULT_SUBMARINE_SERVER_PATH = "mysql+mysqldb://submarine:password@localhost:3306/submarineDB"
 SEARCH_MAX_RESULTS_DEFAULT = 1000
 SEARCH_MAX_RESULTS_THRESHOLD = 50000
